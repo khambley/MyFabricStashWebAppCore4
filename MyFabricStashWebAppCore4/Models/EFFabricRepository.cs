5e923cfebb22994844d024e374aac1cd91cd7609
khambley <superdesigngirl@mac.com>
2019-11-30 08:18:56 -0600

    Prepared the Database, context classes and EF Core

---
+++ MyFabricStashWebAppCore4/Models/EFFabricRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFabricStashWebAppCore4.Models
+{
+    public class EFFabricRepository : IFabricRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFFabricRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Fabric> Fabrics => context.Fabrics;
+    }
+}
