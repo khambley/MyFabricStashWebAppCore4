86c68cf824323afe690a056a08474c330ce01653
khambley <superdesigngirl@mac.com>
2019-12-20 09:20:48 -0600

    Updated ConnectionString config data in appsettings, added Initial Db Migration

---
+++ MyFabricStashWebAppCore4/Migrations/20191220151854_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MyFabricStashWebAppCore4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fabrics",
+                columns: table => new
+                {
+                    FabricId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BackgroundColor = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true),
+                    Colors = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    DateModified = table.Column<DateTime>(nullable: false),
+                    Design = table.Column<string>(nullable: true),
+                    ImagePath = table.Column<string>(nullable: true),
+                    MainCategory = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    SubCategory = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Weight = table.Column<string>(nullable: true),
+                    Width = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fabrics", x => x.FabricId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fabrics");
+        }
+    }
+}
