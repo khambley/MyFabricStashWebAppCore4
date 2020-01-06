55fad8ea6a9ce60921c9ae1d614d58bcbda7b201
khambley <superdesigngirl@mac.com>
2020-01-05 18:01:38 -0600

    Added PagingInfo ViewModel and PageLinkTagHelper classes
    Related Work Items: #86

---
+++ MyFabricStashWebAppCore4.Tests/PageLinkTagHelperTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyFabricStashWebAppCore4.Tests
+{
+    class PageLinkTagHelperTests
+    {
+    }
+}
